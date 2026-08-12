1bcdfddace70c9a46dbd4c0046c2b4bb0aa864ea
gabrielgbeck <gabrielgbeck@gmail.com>
2026-08-11 21:03:35 -0400

    Separated BaseClass and Soldier into separate files in a new folder.

---
+++ Generic Classes/BaseClasses.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FELevelUpCalc;
+
+namespace BaseClasses
+{
+    public class BaseClass(int lvl, int con,
+                           int hp, int hpGrowth,
+                           int str, int strGrowth,
+                           int mag, int magGrowth,
+                           int spd, int spdGrowth,
+                           int skl, int sklGrowth,
+                           int def, int defGrowth,
+                           int res, int resGrowth,
+                           int lck, int lckGrowth)
+    {
+
+        public void LevelUp(int levels)
+        {
+            hp = LevelUpCalculator.LevelUpHP(levels, hp, hpGrowth);
+            str = LevelUpCalculator.LevelUpStat(levels, str, strGrowth);
+            mag = LevelUpCalculator.LevelUpStat(levels, mag, magGrowth);
+            spd = LevelUpCalculator.LevelUpStat(levels, spd, spdGrowth);
+            skl = LevelUpCalculator.LevelUpStat(levels, skl, sklGrowth);
+            def = LevelUpCalculator.LevelUpStat(levels, def, defGrowth);
+            res = LevelUpCalculator.LevelUpStat(levels, res, resGrowth);
+            lck = LevelUpCalculator.LevelUpStat(levels, lck, lckGrowth);
+            lvl += levels;
+            PrintStats();
+        }
+
+        public void PrintStats()
+        {
+            Console.WriteLine("LVL: " + lvl);
+            Console.Write("HP: " + hp + " | ");
+            Console.WriteLine("HP Growth: " + hpGrowth + "%");
+            Console.Write("STR: " + str + " | ");
+            Console.WriteLine("STR Growth: " + strGrowth + "%");
+            Console.Write("MAG: " + mag + " | ");
+            Console.WriteLine("MAG Growth: " + magGrowth + "%");
+            Console.Write("SPD: " + spd + " | ");
+            Console.WriteLine("SPD Growth: " + spdGrowth + "%");
+            Console.Write("SKL: " + skl + " | ");
+            Console.WriteLine("SKL Growth: " + sklGrowth + "%");
+            Console.Write("DEF: " + def + " | ");
+            Console.WriteLine("DEF Growth: " + defGrowth + "%");
+            Console.Write("RES: " + res + " | ");
+            Console.WriteLine("RES Growth: " + resGrowth + "%");
+            Console.Write("LCK: " + lck + " | ");
+            Console.WriteLine("LCK Growth: " + lckGrowth + "%");
+            Console.WriteLine("CON: " + con);
+        }
+
+
+    }
+}
