4d90cbfd9d1d643c868170921f2c87beb9b649a3
gabrielgbeck <gabrielgbeck@gmail.com>
2026-08-12 23:06:46 -0400

    added fighter and knight classes.

---
+++ Generic Classes/Fighter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FELevelUpCalc;
+
+namespace GenericClasses
+{
+    public class Fighter
+    {
+        int lvl = 1;
+        int con = 10;
+        int hp = 20;
+        int hpGrowth = 60;
+        int str = 5;
+        int strGrowth = 50;
+        int mag = 0;
+        int magGrowth = 10;
+        int spd = 3;
+        int spdGrowth = 25;
+        int skl = 3;
+        int sklGrowth = 35;
+        int def = 4;
+        int defGrowth = 35;
+        int res = 2;
+        int resGrowth = 15;
+        int lck = 4;
+        int lckGrowth = 20;
+
+        public void PrintStats()
+        {
+            Console.WriteLine("LVL: " + lvl);
+            Console.Write("HP: " + hp + " | ");
+            Console.WriteLine("HP Growth: " + hpGrowth + "%");
+            Console.Write("STR: " + str + " | ");
+            Console.WriteLine("STR Growth: " + strGrowth + "%");
+            Console.Write("MAG: " + mag + " | ");
+            Console.WriteLine("MAG Growth: " + magGrowth + "%");
+            Console.Write("SPD: " + spd + " | ");
+            Console.WriteLine("SPD Growth: " + spdGrowth + "%");
+            Console.Write("SKL: " + skl + " | ");
+            Console.WriteLine("SKL Growth: " + sklGrowth + "%");
+            Console.Write("DEF: " + def + " | ");
+            Console.WriteLine("DEF Growth: " + defGrowth + "%");
+            Console.Write("RES: " + res + " | ");
+            Console.WriteLine("RES Growth: " + resGrowth + "%");
+            Console.Write("LCK: " + lck + " | ");
+            Console.WriteLine("LCK Growth: " + lckGrowth + "%");
+            Console.WriteLine("CON: " + con);
+        }
+
+        public void LevelUp(int levels)
+        {
+            hp = LevelUpCalculator.LevelUpHP(levels, hp, hpGrowth);
+            str = LevelUpCalculator.LevelUpStat(levels, str, strGrowth);
+            mag = LevelUpCalculator.LevelUpStat(levels, mag, magGrowth);
+            spd = LevelUpCalculator.LevelUpStat(levels, spd, spdGrowth);
+            skl = LevelUpCalculator.LevelUpStat(levels, skl, sklGrowth);
+            def = LevelUpCalculator.LevelUpStat(levels, def, defGrowth);
+            res = LevelUpCalculator.LevelUpStat(levels, res, resGrowth);
+            lck = LevelUpCalculator.LevelUpStat(levels, lck, lckGrowth);
+            lvl += levels;
+            PrintStats();
+        }
+    }
+}
---
+++ Generic Classes/Knight.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FELevelUpCalc;
+
+namespace GenericClasses
+{
+    public class Knight
+    {
+        int lvl = 1;
+        int con = 8;
+        int hp = 16;
+        int hpGrowth = 70;
+        int str = 5;
+        int strGrowth = 40;
+        int mag = 0;
+        int magGrowth = 10;
+        int spd = 2;
+        int spdGrowth = 25;
+        int skl = 4;
+        int sklGrowth = 25;
+        int def = 7;
+        int defGrowth = 40;
+        int res = 4;
+        int resGrowth = 30;
+        int lck = 3;
+        int lckGrowth = 10;
+
+        public void PrintStats()
+        {
+            Console.WriteLine("LVL: " + lvl);
+            Console.Write("HP: " + hp + " | ");
+            Console.WriteLine("HP Growth: " + hpGrowth + "%");
+            Console.Write("STR: " + str + " | ");
+            Console.WriteLine("STR Growth: " + strGrowth + "%");
+            Console.Write("MAG: " + mag + " | ");
+            Console.WriteLine("MAG Growth: " + magGrowth + "%");
+            Console.Write("SPD: " + spd + " | ");
+            Console.WriteLine("SPD Growth: " + spdGrowth + "%");
+            Console.Write("SKL: " + skl + " | ");
+            Console.WriteLine("SKL Growth: " + sklGrowth + "%");
+            Console.Write("DEF: " + def + " | ");
+            Console.WriteLine("DEF Growth: " + defGrowth + "%");
+            Console.Write("RES: " + res + " | ");
+            Console.WriteLine("RES Growth: " + resGrowth + "%");
+            Console.Write("LCK: " + lck + " | ");
+            Console.WriteLine("LCK Growth: " + lckGrowth + "%");
+            Console.WriteLine("CON: " + con);
+        }
+
+        public void LevelUp(int levels)
+        {
+            hp = LevelUpCalculator.LevelUpHP(levels, hp, hpGrowth);
+            str = LevelUpCalculator.LevelUpStat(levels, str, strGrowth);
+            mag = LevelUpCalculator.LevelUpStat(levels, mag, magGrowth);
+            spd = LevelUpCalculator.LevelUpStat(levels, spd, spdGrowth);
+            skl = LevelUpCalculator.LevelUpStat(levels, skl, sklGrowth);
+            def = LevelUpCalculator.LevelUpStat(levels, def, defGrowth);
+            res = LevelUpCalculator.LevelUpStat(levels, res, resGrowth);
+            lck = LevelUpCalculator.LevelUpStat(levels, lck, lckGrowth);
+            lvl += levels;
+            PrintStats();
+        }
+    }
+}
